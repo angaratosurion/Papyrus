4b04902ef55751ee9edaa42f1d276993ae8ad407
angaratosurion <angarato.surion@gmail.com>
2018-06-11 21:24:55 +0300

    g
    g

---
+++ Papyrus.Core/SDK/BaseClasses/UI/WindowPlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Papyrus.Core.SDK.BaseClasses.UI
+
+{
+    public class WindowPlugin 
+    {
+        Boolean isToolbar { get; }
+        void AddPluginToWindow(Window win) { };
+
+
+    }
+}
